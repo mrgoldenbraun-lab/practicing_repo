ba507cbe401b336f4ca5ffaaf4b527ac721ffa7a
Vincent Wisely <vincent.wisely@binus.ac.id>
2025-08-29 22:50:08 +0700

    make the program didn't close instantly when double clicked

--- assignment.c
+++ assignment.c
@@ -3,5 +3,7 @@
 int main(){
     printf("Hello, My name is Vincent Wisely!\n");
     printf("My favorite programming is c and c#\n");
+    printf("\nPress Enter to exit...");
+    getchar();
     return 0;
 }
