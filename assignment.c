c0e86fab21f304bf01d7738b6eaee30feb1f3e6f
Vincent Wisely <vincent.wisely@binus.ac.id>
2025-08-29 22:43:41 +0700

    fix assignment.c and make assignment.exe

--- assignment.c
+++ assignment.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello, My name is Vincent Wisely!/n");
-    printf("My favorite programming is c and c#/n");
+    printf("Hello, My name is Vincent Wisely!\n");
+    printf("My favorite programming is c and c#\n");
     return 0;
 }
